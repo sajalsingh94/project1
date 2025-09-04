a51df145198710b03b9552577b141dbc69298759
sajalsingh94 <132097830+sajalsingh94@users.noreply.github.com>
2025-09-04 19:34:11 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import ShopsPage from "./pages/ShopsPage";
+import SellerDetailPage from "./pages/SellerDetailPage";
+import RecipesPage from "./pages/RecipesPage";
+import AboutPage from "./pages/AboutPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      cacheTime: 1000 * 60 * 10, // 10 minutes
+      retry: 2
+    }
+  }
+});
+
+const App = () =>
+<QueryClientProvider client={queryClient}>
+    <HelmetProvider>
+      <TooltipProvider>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/shops" element={<ShopsPage />} />
+              <Route path="/seller/:sellerId" element={<SellerDetailPage />} />
+              <Route path="/recipes" element={<RecipesPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+        <Toaster />
+      </TooltipProvider>
+    </HelmetProvider>
+  </QueryClientProvider>;
+
+
+export default App;
